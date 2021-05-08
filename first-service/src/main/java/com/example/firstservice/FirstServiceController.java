47756a981741622cb273e318564b8eb3a201faf8
hbchae <gusqud87@naver.com>
2021-05-08 12:23:50 +0900

    Spring Cloud Gateway - Custom Filter 적용 마지막

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -17,7 +17,12 @@ public class FirstServiceController {
 
     @GetMapping("/message")
     public String message(@RequestHeader("first-request") String header) {
-      log.info(header);
-      return "Hello World in First Service";
+        log.info(header);
+        return "Hello World in First Service";
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Hi, there. This is a message from First Service";
     }
 }
